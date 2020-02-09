291ce76820bc2d86530ab908796bde07bc3cc197
Azizun Ussama <56993220+Azizun42@users.noreply.github.com>
2020-02-09 23:15:59 +0700

    Add files via upload

---
+++ Pokemon.java
@@ -0,0 +1,223 @@
+import java.io.*;
+import java.util.*;
+
+public class Pokemon
+{
+	private String name;
+	private int hp;
+	private int level;
+	private int exp;
+	private int attack;
+	
+  
+  public Pokemon(){
+        //Hp start at 50
+        hp = 50 ;
+        exp = 0 ;
+        attack = 100 ;
+	}
+	
+	
+  
+  public String setName(){
+        name = "David";
+      return name ;
+	} 
+	
+  
+  public  changeName(String newName){
+		name = newName;
+	}   
+
+
+	
+  
+  
+
+
+ }
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import Pokemon.*;
+ 
+public class  main {
+    public static void main(String[] args) {
+        System.out.println("Hello Welcome ");
+        System.out.println("-----------------------------");
+        System.out.println("Pokemon Game !!!!!!!!!!!!!!!!!!!");
+        System.out.println("*******Start Game*******");
+        Pokemon pk1 = new Pokemon();
+        System.out.println("Now your pokemon status");
+        System.out.println("Hp is :" + pk1.hp);
+
+        
+        
+
+    }
+}
