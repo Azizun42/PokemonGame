39ef4e23cbc8816dbf8af5ccb22edf1c736f4b24
Azizun Ussama <56993220+Azizun42@users.noreply.github.com>
2020-02-10 14:07:07 +0700

    Update main.java

--- main.java
+++ main.java
@@ -3,13 +3,13 @@ import Pokemon.*;
  
 public class  main {
     public static void main(String[] args) {
-        System.out.println("Hello Welcome ");
+        System.out.println("Hello Welcome to ");
         System.out.println("-----------------------------");
         System.out.println("Pokemon Game !!!!!!!!!!!!!!!!!!!");
         System.out.println("*******Start Game*******");
         Pokemon pk1 = new Pokemon();
         System.out.println("Now your pokemon status");
-        System.out.println("Hp is :" + pk1.hp);
+        
 
         
         
